ea2cf784874a05ed39d3f89d3a3b97ba990292c8
Felix Blix Everberg <felixblix@gmail.com>
2016-08-29 14:20:21 +0200

    more questions and test of * and -> -felix

--- shared_ptr/main.cpp
+++ shared_ptr/main.cpp
@@ -5,5 +5,15 @@ int main() {
     std::string hello_world = "Hello World";
     SharedPtr<std::string, int> mySharedPtr = SharedPtr<std::string, int>(&hello_world);
 
+    //overload tests:
+    typedef struct TestStruct{
+      std::string a = "hello"
+      std::string b = "world"
+    } TestStruct;
+
+    SharedPtr<TestStruct> mySharedPtr = SharedPtr<TestStruct>(TestStruct hw_struct );
+    stdout << "(*mySharedPtr).a << (*mySharedPtr).b returned: " << (*mySharedPtr).a << (*mySharedPtr).b << newline;
+    stdout << "mySharedPtr->a << mySharedPtr->b returned: " << mySharedPtr->a << mySharedPtr->b << newline;
+    
     return 0;
 }
