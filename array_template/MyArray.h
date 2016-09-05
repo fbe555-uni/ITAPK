12b7860b36b3e47bceb632878de5161395a5cf22
Felix Blix Everberg <felixblix@gmail.com>
2016-09-05 12:53:19 +0200

    Basic impl of template array -Felix

--- array_template/MyArray.h
+++ array_template/MyArray.h
@@ -8,39 +8,46 @@
 #define ARRAY_TEMPLATE_MYARRAY_H
 namespace felhak {
 
-    template<typename T>
+    template<typename T, size_t size>
     class MyArray {
     public:
-        MyArray(size_t s){
-            _size = s;
-        }
+        MyArray(){}
 
-        ~MyArray() {
+        ~MyArray() {}
 
-        }
-
-        void fill(const T &) {
-
+        void fill(const T & value) {
+            T* i = begin();
+            for(; i != end(); i++){
+                *i = value;
+            }
         }
 
         T *begin() const {
-
+            return array;
         }
 
         T *end() const {
-
+            return array+_size;
         }
 
-        T &operator[](int i) {
-
+        T& operator[](int i) {
+            return *(array+i);
         }
 
         size_t size() const {
-
+            return _size;
         }
 
     private:
-        size_t _size;
-        T array[];
+        size_t _size = size;
+        T array[size];
     };
+
+    template <typename T>
+    T* myfind (T* first , T* last , const T& v){
+        for(;first != last; first++){
+            if(*first == v) return first;
+        }
+        return nullptr;
+    }
 }
