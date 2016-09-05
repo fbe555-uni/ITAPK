a471a66d3f646f0d203598c070f95c0a45bb9462
Felix Blix Everberg <felixblix@gmail.com>
2016-09-05 13:32:58 +0200

    Basic impl of template array -Felix

--- array_template/MyArray.h
+++ array_template/MyArray.h
@@ -8,16 +8,47 @@
 #define ARRAY_TEMPLATE_MYARRAY_H
 namespace felhak {
 
-    template<typename T, size_t size>
+    template<typename T, size_t size_>
     class MyArray {
     public:
-        MyArray(){}
+        MyArray(){
+            array = new T[size_];
+        }
 
-        ~MyArray() {}
+        template<typename D>
+        MyArray(MyArray<D, size_t> orig){
+            array = new T[orig.size()];
+            _size = orig.size();
+            T* i = begin();
+            D* j = orig.begin();
+            while(i != end()){
+                *i = *j;
+                i++;
+                j++;
+            }
+        }
+
+        template <typename D>
+        operator=(MyArray<D, size_t> orig){
+            if(_size != orig.size()) {
+                array = new T[orig.size()];
+                _size = orig.size();
+                T* i = begin();
+                D* j = orig.begin();
+                while(i != end()){
+                    *i = *j;
+                    i++;
+                    j++;
+                }
+            }
+        }
+
+        ~MyArray() {
+            delete array;
+        }
 
         void fill(const T & value) {
-            T* i = begin();
-            for(; i != end(); i++){
+            for(T* i = begin(); i != end(); i++){
                 *i = value;
             }
         }
@@ -39,8 +70,8 @@ namespace felhak {
         }
 
     private:
-        size_t _size = size;
-        T array[size];
+        size_t _size = size_;
+        T* array;
     };
 
     template <typename T>
