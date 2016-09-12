074fb9ce17d13daf8023366c46ae9e324b4cd66f
Felix Blix Everberg <felixblix@gmail.com>
2016-09-12 08:43:59 +0200

    added new exercise folders. -felix

---
+++ ex1-3/shared_ptr/build/CMakeFiles/CMakeTmp/CMakeFiles/cmTryCompileExec1198481710.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTryCompileExec1198481710.dir/feature_tests.cxx.o"
+  "cmTryCompileExec1198481710.pdb"
+  "cmTryCompileExec1198481710"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTryCompileExec1198481710.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
