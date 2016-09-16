6170951f9a3212fc2a598410f1b2d9bbc629478a
huxxcb <hakonprimdahl@gmail.com>
2016-09-16 14:56:54 +0200

    done calctotalsold -h

--- ex5/ProductSystem/algo.cpp
+++ ex5/ProductSystem/algo.cpp
@@ -13,64 +13,57 @@
 #include <vector>
 #include <string>
 
-class Product
-{
+class Product {
 public:
-    Product(const std::string& name, float price, unsigned int sold = 0)
-            : name_(name), price_(price), sold_(sold)
-    {}
-    Product()
-            : name_(""), price_(0), sold_(0)
-    {}
+    Product(const std::string &name, float price, unsigned int sold = 0)
+            : name_(name), price_(price), sold_(sold) {}
 
-    const std::string& name() const
-    {
+    Product()
+            : name_(""), price_(0), sold_(0) {}
+
+    const std::string &name() const {
         return name_;
     }
 
-    float price() const
-    {
+    float price() const {
         return price_;
     }
 
-    void setPrice(float newPrice)
-    {
+    void setPrice(float newPrice) {
         price_ = newPrice;
     }
 
-    unsigned int sold() const
-    {
+    unsigned int sold() const {
         return sold_;
     }
 
-    friend std::istream& operator>> ( std::istream& i, Product& p );
-    friend std::ostream& operator<< ( std::ostream& o, const Product& p );
+    friend std::istream &operator>>(std::istream &i, Product &p);
+
+    friend std::ostream &operator<<(std::ostream &o, const Product &p);
 
 private:
-    std::string   name_;
-    float         price_;
-    int           sold_;
+    std::string name_;
+    float price_;
+    int sold_;
 };
-typedef std::vector<Product>  ProductList;
+
+typedef std::vector<Product> ProductList;
 
 
-std::ostream& operator<< ( std::ostream& o, const Product& p )
-{
+std::ostream &operator<<(std::ostream &o, const Product &p) {
     return o << p.name_ << " " << p.price_ << " " << p.sold_;
 }
 
-std::istream& operator>> ( std::istream& i, Product& p )
-{
+std::istream &operator>>(std::istream &i, Product &p) {
     return i >> p.name_ >> p.price_ >> p.sold_;
 }
 
 /**
  * Read products from file
  */
-void productDBRead(ProductList& pl, const std::string& fileName)
-{
+void productDBRead(ProductList &pl, const std::string &fileName) {
     pl.clear();
-    std::ifstream pFile( fileName.c_str() );
+    std::ifstream pFile(fileName.c_str());
     std::istream_iterator<Product> begin_it(pFile);
     std::istream_iterator<Product> end_it;
     std::copy(begin_it, end_it, std::back_inserter(pl));
@@ -87,8 +80,7 @@ void productDBRead(ProductList& pl, const std::string& fileName)
 /**
  * Printout all products
  */
-void printAll(const ProductList& pl)
-{
+void printAll(const ProductList &pl) {
     std::cout << "##################################################" << std::endl;
     std::cout << "Printing out all products..." << std::endl;
     std::cout << "----------------------------" << std::endl;
@@ -102,12 +94,10 @@ void printAll(const ProductList& pl)
 }
 
 
-
 /**
    Add item
 */
-void addItem(ProductList& pl)
-{
+void addItem(ProductList &pl) {
     std::string productName;
     float productPrice;
     std::cout << "Please enter the product name: ";
@@ -122,18 +112,18 @@ void addItem(ProductList& pl)
 /**
    Write data to db file
 */
-void productDBWrite(const ProductList& pl, const std::string& fileName)
-{
+void productDBWrite(const ProductList &pl, const std::string &fileName) {
     std::ofstream pFile(fileName.c_str());
     std::copy(pl.begin(), pl.end(), std::ostream_iterator<Product>(pFile, "\n"));
 }
 
-struct salesComp
-{
-    salesComp(const int sales) : _sales(sales){}
-    bool operator()(const Product& p){
+struct salesComp {
+    salesComp(const int sales) : _sales(sales) {}
+
+    bool operator()(const Product &p) {
         return p.sold() > _sales;
     }
+
     int _sales;
 };
 
@@ -145,8 +135,7 @@ struct salesComp
 /**
  * Print poorly selling products
  */
-void printPoorlySellingProducts(const ProductList& pl)
-{
+void printPoorlySellingProducts(const ProductList &pl) {
     std::ostream_iterator<Product> out_it(std::cout, "\n\r");
 
     std::remove_copy_if(pl.begin(),
@@ -158,29 +147,33 @@ void printPoorlySellingProducts(const ProductList& pl)
 /***********************************************************
  *                    Discount functor                     *
  ***********************************************************/
-struct DiscountFunctor{
-    DiscountFunctor(float percentDiscount): _dis((1-(percentDiscount/100))){}
-    void operator()(Product& p) {
+struct DiscountFunctor {
+    DiscountFunctor(float percentDiscount) : _dis((1 - (percentDiscount / 100))) {}
+
+    void operator()(Product &p) {
         p.setPrice(p.price() * _dis);
     }
+
     float _dis;
 };
 
-struct DiscountTransFunctor{
-    DiscountTransFunctor(float percentDiscount): _dis((1-(percentDiscount/100))){}
-    Product operator()(Product& p){
+struct DiscountTransFunctor {
+    DiscountTransFunctor(float percentDiscount) : _dis((1 - (percentDiscount / 100))) {}
+
+    Product operator()(Product &p) {
         Product new_p = Product(p);
-        new_p.setPrice(p.price()*_dis);
+        new_p.setPrice(p.price() * _dis);
         return new_p;
 
     }
+
     float _dis;
 };
+
 /**
  * Set a discount on all products - Using for_each()
  */
-void addDiscountUsingForEach(ProductList& pl, float percentDiscount)
-{
+void addDiscountUsingForEach(ProductList &pl, float percentDiscount) {
     std::for_each(pl.begin(), pl.end(), DiscountFunctor(percentDiscount));
 }
 
@@ -188,19 +181,39 @@ void addDiscountUsingForEach(ProductList& pl, float percentDiscount)
 /**
  * Set a discount on all products - Using transform()
  */
-void addDiscountUsingTransform(ProductList& pl, float percentDiscout)
-{
-    std::cout << "#########################################################" << std::endl << "Printing discounted product list: " << std::endl;
-    std::transform(pl.begin(),pl.end(), std::ostream_iterator<Product>(std::cout, "\n\r"), DiscountTransFunctor(percentDiscout));
-    std::cout << "Please note this discount is not applied to the actual product list!" << std::endl << "#########################################################" << std::endl;
+void addDiscountUsingTransform(ProductList &pl, float percentDiscout) {
+    std::cout << "#########################################################" << std::endl
+              << "Printing discounted product list: " << std::endl;
+    std::transform(pl.begin(), pl.end(), std::ostream_iterator<Product>(std::cout, "\n\r"),
+                   DiscountTransFunctor(percentDiscout));
+    std::cout << "Please note this discount is not applied to the actual product list!" << std::endl
+              << "#########################################################" << std::endl;
 }
 
+struct TotalSoldFunctor{
+    TotalSoldFunctor();
+
+    int operator()(Product &p) {
+        return p.sold();
+    }
+};
 
 /**
  * Calculate the total amount of sold products
  */
-void calcTotalSoldProducts(ProductList& pl)
-{
+void calcTotalSoldProducts(ProductList &pl) {
+    std::vector<unsigned int> sold_product_count;
+    std::cout << "########################################################" << std::endl
+              << "Printing total amount of sould products" << std::endl;
+    std::transform(pl.begin(), pl.end(), std::back_inserter(sold_product_count), std::mem_fun_ref(&Product::sold)); /////sold_product_count);
+    int accum = std::accumulate(sold_product_count.begin(),
+                    sold_product_count.end(),
+                    0,
+                    [](int a, int b) {
+                        return a + b;
+                    });
+    std::cout << accum << std::endl;
+    std::cout << "########################################################" << std::endl;
 }
 
 
