0bba4c1e889f5cbea1f0a674b49f805b87977cbf
Felix Blix Everberg <felixblix@gmail.com>
2016-09-19 12:22:18 +0200

    fixed ex6 timer not to alter the Event class

--- ex6/timer_callback/Bindfunction.cpp
+++ ex6/timer_callback/Bindfunction.cpp
@@ -15,12 +15,18 @@
 
 std::mutex stdioProt;
 
-
 void freeFunction(const std::shared_ptr<Event> &event) {
     // Protecting stdio since its not thread safe
     std::lock_guard<std::mutex> lock(stdioProt);
-
-    std::cout << "freeFunction received the following event: " << *event << std::endl;
+    std::cout << "freeFunction received the following "
+    if(typeid(*event) == typeid(Event)) {
+        std::cout << "Event: ";
+    }else if(typeid(*event) == typeid(EventOther)){
+        std::cout << "EventOther: ";
+    }else{
+        std::cout << "Unknown Event derivation: ";
+    }
+    std::cout << *event << std::endl;
 }
 
 
@@ -28,10 +34,18 @@ void withAnExtra(const std::shared_ptr<Event> &event, const std::string text) {
     // Protecting stdio since its not thread safe
     std::lock_guard<std::mutex> lock(stdioProt);
 
-    std::cout << "withAnExtra received the following event: " << *event << std::endl;
+    std::cout << "withAnExtra called. forwarding to freeFunction:" << std::endl;
+    freeFunction(event);
     std::cout << "With the following extra: " << text << std::endl;
 
 }
+
+struct testFunctor{
+    void operator()(const std::shared_ptr<Event> &event){
+        std::cout << "Functor called. forwarding to freeFunction:" << std::endl;
+        freeFunction(event);
+    }
+};
 
 
 class ReferenceObj {
@@ -43,7 +57,8 @@ public:
         // Protecting stdio since its not thread safe
         std::lock_guard<std::mutex> lock(stdioProt);
 
-        std::cout << "ReferenceObj received the following event: " << *event << std::endl;
+        std::cout << "ReferenceObj called. forwarding to freeFunction:" << std::endl;
+        freeFunction(event);
         std::cout << "This is the " << ++called_ << "time this obj has been called" << std::endl;
     }
 
@@ -60,7 +75,9 @@ int main() {
     // Try to make several timers with different callbacks
     Timer t1(4);
     t1.attach(freeFunction);
-
+    testFunctor tf;
+    t1.attach(tf);
+    t1.attach(boost::bind())
 
 
     // The threads run naturally in the background (no clean up has been added for the threads)
--- ex6/timer_callback/Events.cpp
+++ ex6/timer_callback/Events.cpp
@@ -10,10 +10,3 @@ std::ostream &operator<<(std::ostream &os, const Event &ev) {
     return os;
 }
 
-std::ostream &operator<<(std::ostream &os, const EventOther &ev) {
-    std::time_t creationTime = std::chrono::system_clock::to_time_t(ev.creationTime());
-    os << "EventOther: " << std::ctime(&creationTime);
-
-    return os;
-}
-
